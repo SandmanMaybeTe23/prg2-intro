91ed932768acb1f89597e79a8f2367fda3d3b9ae
SandmanMaybeTe23 <billt.burlin@elev.ga.ntig.se>
2025-08-22 10:42:46 +0200

    first commit

---
+++ omelet.py
@@ -0,0 +1,23 @@
+omelet=0
+ägg_per_omelet=2
+egg_destroyed=0
+
+omelet=int(input("hur många omeleter vi du ha"))
+ägg_per_omelet=int(input("hur många egg per omelet "))
+
+for i in range(0,omelet):
+    egg_destroyed+=ägg_per_omelet
+    omelet+=1
+
+print (f"du förstöde",[egg_destroyed]," för att få",[omelet]," hur mår du Rasmus")
+
+   
+
+
+    
+
+
+
+
+
+ 
