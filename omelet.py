774623b360c3e829bca453503a4c8436f1c95773
SandmanMaybeTe23 <billt.burlin@elev.ga.ntig.se>
2025-08-22 10:47:17 +0200

    done

--- omelet.py
+++ omelet.py
@@ -2,14 +2,14 @@ omelet=0
 ägg_per_omelet=2
 egg_destroyed=0
 
-omelet=int(input("hur många omeleter vi du ha"))
+omelet=int(input("hur många omeleter vi du ha "))
 ägg_per_omelet=int(input("hur många egg per omelet "))
 
 for i in range(0,omelet):
     egg_destroyed+=ägg_per_omelet
     omelet+=1
 
-print (f"du förstöde",[egg_destroyed]," för att få",[omelet]," hur mår du Rasmus")
+print (f"du förstöde",[egg_destroyed]," för att få",[omelet])
 
    
 
